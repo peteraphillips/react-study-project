0444ca6f23e33bc11fee341ef19bce5bb4402164
peteraphillips <p.phillips59311@gmail.com>
2024-01-18 14:40:31 +0000

    step 1.6 completed (got it right first time too I might add) ;)

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,14 +1,34 @@
 import { useState } from 'react'
 
+const Header = () => <h1>Give Feedback</h1>
+
+const Display = props => <div>{props.value}</div>
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const setValue = newValue => {
+    console.log('value now', newValue)
+    setValue(newValue)
+  }
+
   return (
     <div>
-      code here
+      <Header />
+      <Display value={good} />
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Display value={neutral} />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Display value={bad} />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
     </div>
   )
 }
