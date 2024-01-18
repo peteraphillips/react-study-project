407d5365a27e9f3c0951f68d28cff5a710735e4b
peteraphillips <p.phillips59311@gmail.com>
2024-01-18 14:10:43 +0000

    starting point for task 1.6

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,73 +1,14 @@
+import { useState } from 'react'
+
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      <Header course={course} />
-
-      <Content parts={parts} />
-
-      <Total parts={parts} />
-    </div>
-  )
-}
-
-const Header = (props) => {
-  return (
-    <div>
-      <h1>
-        Course: {props.course}
-      </h1>
-    </div>
-  )
-}
-
-const Content = (props) => {
-    return (
-      <div>
-        <Part part={props.parts[0]}/>
-        <Part part={props.parts[1]}/>
-        <Part part={props.parts[2]}/>
-      </div>
-    )
-}
-
-const Part = (props) => {
-  console.log(props.part)
-
-    return (
-      <>
-        <p>{props.part.name} {props.part.exercises}</p>
-      </>
-    )
-
-}
-
-const Total = (props) => {
-
-  let y = 0
-
-  props.parts.forEach(value => {
-    y += value.exercises
-  })
-
-  return (
-    <div>
-      <p>Number of exercises: {y}</p>
+      code here
     </div>
   )
 }
