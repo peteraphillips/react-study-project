a38aee53c80ebea81073509a3b79e3cd2751a25d
peteraphillips <p.phillips59311@gmail.com>
2024-03-31 13:10:26 +0100

    completed exercixe 2.10 step 5

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,28 +1,12 @@
 import { useState } from 'react'
-// import Search from './components/search'
 
-const App = () => {
+const Search = (props) => {
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  var persons = props.persons
 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
   const [newSearch, setNewSearch] = useState('')
   const [searchedPersons, setSearchedPersons] = useState('')
-  // const [showAll, setShowAll] = useState(true)
 
-  const handleNewName = (event) => {
-    setNewName(event.target.value)
-  }
-
-  const handleNewNumber = (event) => {
-    setNewNumber(event.target.value)
-  }
 
   const handleNewSearch = (event) => {
     event.preventDefault()
@@ -33,10 +17,57 @@ const App = () => {
   const search = (event) => {
     event.preventDefault()
 
+
     setSearchedPersons(persons.filter(i => i.name.includes(newSearch)))
     setNewSearch('')
   }
 
+  return (
+    <div>
+      <form>
+        <div>
+          search: <input 
+            value={newSearch}
+            onChange={handleNewSearch}
+          />
+        </div>
+        <div>
+          <button onClick={search}>Search</button>
+        </div>
+      </form>
+      {searchedPersons.length > 0
+      ? searchedPersons.map(i =>
+      <div key={i.number}>
+        <p>
+            Name: {i.name}{' '}
+        </p>
+        <p>
+            Number: {i.number}
+        </p>
+        <hr/>
+      </div>
+      )
+      : ''
+      }
+
+    </div>
+  )
+}
+
+const PersonForm = (props) => {
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+  var persons = props.persons
+  var setPersons = props.setPersons
+
+  const handleNewName = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNewNumber = (event) => {
+    setNewNumber(event.target.value)
+  }
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -54,49 +85,36 @@ const App = () => {
     
     setNewName('')
     setNewNumber('')
-    setNewSearch('')
   }
 
   return (
-    <div>
-      <h2>Phonebook</h2>
-      <form>
-        <div>
-          search: <input 
-            value={newSearch}
-            onChange={handleNewSearch}
-            onClick={search}
+    <form>
+      <div>
+          name: <input 
+          value={newName}
+          onChange={handleNewName}
           />
-        </div>
-        <div>
-          <button onClick={search}>Search</button>
-        </div>
-      </form>
-
-      <div>
-
       </div>
-      <form>
-        <div>
-            name: <input 
-            value={newName}
-            onChange={handleNewName}
-            />
-        </div>
-        <div>
-            number: <input 
-            value={newNumber}
-            onChange={handleNewNumber}  
-            />
-        </div>
-        <div>
-            <button onClick={addPerson}>add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
       <div>
-      {searchedPersons
-      ? searchedPersons.map(person => 
+          number: <input 
+          value={newNumber}
+          onChange={handleNewNumber}  
+          />
+      </div>
+      <div>
+          <button onClick={addPerson}>add</button>
+      </div>
+    </form>
+  )
+}
+
+const Persons = (props) => {
+
+  var persons = props.persons
+
+  return (
+    <div>
+    {persons.map(person => 
         <div key={person.number}>
         <p>
             Name: {person.name}{' '}
@@ -106,20 +124,33 @@ const App = () => {
         </p>
         <hr/>
         </div>
-      )
-      : persons.map(person => 
-          <div key={person.number}>
-          <p>
-              Name: {person.name}{' '}
-          </p>
-          <p>
-              Number: {person.number}
-          </p>
-          <hr/>
-          </div>
-      )}
-      </div>
-  </div>
+    )}
+    </div>
+  )
+
+}
+
+const App = () => {
+
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ]) 
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <Search persons={persons}/>    
+
+      <PersonForm persons={persons} setPersons={setPersons}/>  
+
+      <h2>Numbers</h2>
+
+      <Persons persons={persons} />
+    </div>
   )
 }
 
