6e48d8b4e35623c157a852b336dc181c256ba8bd
peteraphillips <p.phillips59311@gmail.com>
2024-05-23 14:06:18 +0100

    attempted testing (struggled)

---
+++ blog/utils/list_helper.js
@@ -0,0 +1,20 @@
+const dummy = (blogs) => {
+    return 1
+}
+
+const totalLikes = (blogs) => {
+
+    var likes = 0
+
+    blogs.forEach(blog => {
+        likes += blog.likes
+    });
+
+    return likes
+}
+
+module.exports = {
+    dummy,
+    totalLikes
+}
+
